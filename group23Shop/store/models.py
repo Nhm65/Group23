2b8ba87f3c1b975ad7913d4fe667490f53845e41
cameron226fr <124012431+cameron226fr@users.noreply.github.com>
2023-04-29 17:12:21 +0300

    Added DB Table Models
    Added DB Table Models

--- group23Shop/store/models.py
+++ group23Shop/store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Orders(models.Model):
+    id=models.AutoField
+    customer_id=models.CharField(max_length=255)
+    order_date=models.DateTimeField
+    total_amount=models.CharField(max_length=255)
+
+class Order_Details(models.Model):
+    id=models.AutoField
+    order_id=models.CharField(max_length=255)
+    product_id=models.CharField(max_length=255)
+    quantity=models.CharField(max_length=255)
+    price=models.CharField(max_length=255)
 # Create your models here.
