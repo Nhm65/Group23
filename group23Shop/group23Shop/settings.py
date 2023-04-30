ffd5f4c654f68258de0a5834aa262784dd3ad86b
cameron226fr <124012431+cameron226fr@users.noreply.github.com>
2023-04-30 06:06:47 +0300

    Updated MySQL Database Configurations

--- group23Shop/group23Shop/settings.py
+++ group23Shop/group23Shop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'group23Shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clientorders',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
